5247647dcc0cc1466dc89840fb9d059856f2476c
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-05-20 18:40:36 +0100

    Starter activity solution

--- src/main/java/com/kenny/wenny/data/structures/Main.java
+++ src/main/java/com/kenny/wenny/data/structures/Main.java
@@ -1,17 +1,22 @@
 package com.kenny.wenny.data.structures;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /*
+An array of Strings
+    And an nth element of this (just pick any index)
+A Hashmap of animal types & their names
+    E.g. dog = mary, cat = kate
+         */
+        String[] someStrings = { "Violeta", "Jessie", "Rads", "Nade", "Ekta", "Paula" };
+        System.out.println(someStrings[4]);
+        Map<String, String> animalMap = new HashMap<>();
+        animalMap.put("dog", "mary");
+        animalMap.put("cat", "kate");
     }
 }
